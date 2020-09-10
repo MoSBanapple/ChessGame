cf7a69c8b7d65547806ae4c29ef4b9813436bb4d
Derek Zhang <derekz1234@gmail.com>
2020-09-10 13:54:08 -0700

    Updates

---
+++ precache-manifest.9fd4b6d8987156755c2e77546330fcfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "486956e0545de52661016fa6085d42a0",
+    "url": "/ChessGame/index.html"
+  },
+  {
+    "revision": "9394bb6d37b199a03871",
+    "url": "/ChessGame/static/css/main.7ecb3ff7.chunk.css"
+  },
+  {
+    "revision": "33d95957f9149741e0df",
+    "url": "/ChessGame/static/js/2.9830d106.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ChessGame/static/js/2.9830d106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9394bb6d37b199a03871",
+    "url": "/ChessGame/static/js/main.be822b34.chunk.js"
+  },
+  {
+    "revision": "f49b550f04bf74f7f574",
+    "url": "/ChessGame/static/js/runtime-main.4f71bf9a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ChessGame/static/media/logo.5d5d9eef.svg"
+  }
+]);
